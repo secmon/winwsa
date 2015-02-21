86297670c7c9e5757a1c4f25d3f1668e5b1d3db2
zhylik <ivan.a.zhilin@ya.ru>
2015-02-21 16:50:35 +0300

    rename WinWSAParserAutorunsBin

--- WinWSA/Parser/WinWSAAutorunsBinParser.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace WinWSA\Parser;
-
-class WinWSAAutorunsBinParser {
-
-}
---
+++ WinWSA/Parser/WinWSAParserAutorunsBin.php
@@ -0,0 +1,7 @@
+<?php
+namespace WinWSA\Parser;
+
+class WinWSAParserAutorunsBin {
+	public function __construct() {
+	}
+}
