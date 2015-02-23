cd52edb9a87674cc9ab22908b3ed24d10305a203
zhylik <ivan.a.zhilin@ya.ru>
2015-02-23 15:00:55 +0300

    tests

--- tests/Parser/AutorunsXML13Parser.php
+++ tests/Parser/AutorunsXML13Parser.php
@@ -1,6 +1,13 @@
 <?php
 
+namespace WinWSA\Parser;
+
+require '..\WinWSA\Parser\AutorunsXML13Parser.php';
+
 class AutorunsXML13ParserTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testParsingOfExampleReports() 
+    {
+        
+    }
 }
