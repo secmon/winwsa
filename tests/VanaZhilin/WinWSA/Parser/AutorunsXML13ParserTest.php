8092f50db63632ab0ed557911a6feae2e0f91617
vanazhilin <ivan.a.zhilin@ya.ru>
2015-02-23 19:58:18 +0300

    ref

--- tests/VanaZhilin/WinWSA/Parser/AutorunsXML13ParserTest.php
+++ tests/VanaZhilin/WinWSA/Parser/AutorunsXML13ParserTest.php
@@ -15,6 +15,7 @@ class AutorunsXML13ParserTest extends \PHPUnit_Framework_TestCase
 
     public function testParseAutoruns1301XMLt() {
         $parser = new AutorunsXML13Parser();
+        
         $items = $parser->parse(dirname(__FILE__) .'/../../../fixtures/autoruns13.01-xmlt.xml');
         $this->assertEquals('0cae1d927cbf8ecaa7e7974a1a04c323', md5(serialize($items)));
 
