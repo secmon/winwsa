1fbde06a190e29fb5faa8bb76c54e7e97a35b977
vanazhilin <ivan.a.zhilin@ya.ru>
2015-02-23 16:40:20 +0300

    tests

--- tests/Parser/AutorunsXML13ParserTest.php
+++ tests/Parser/AutorunsXML13ParserTest.php
@@ -6,6 +6,7 @@ class AutorunsXML13ParserTest extends \PHPUnit_Framework_TestCase
 {
     public function testParsingOfExampleReports() 
     {
-
+    	$parser = new AutorunsXML13Parser();
+    	$parser->parse(dirname(__FILE__));
     }
 }
