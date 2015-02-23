e414e59c2a8506f58184734b94b55a6c5b93c74b
vanazhilin <ivan.a.zhilin@ya.ru>
2015-02-23 18:08:14 +0300

    +testParseAutoruns1301XMLt

--- tests/VanaZhilin/WinWSA/Parser/AutorunsXML13ParserTest.php
+++ tests/VanaZhilin/WinWSA/Parser/AutorunsXML13ParserTest.php
@@ -4,12 +4,17 @@ namespace VanaZhilin\WinWSA\Parser;
 
 class AutorunsXML13ParserTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @expectedException \Exception
-	 */
+    /**
+     * @expectedException \Exception
+     */
     public function testParseUnexistentFile() 
     {
-    	$parser = new AutorunsXML13Parser();
-    	$parser->parse('unexistredfile210573');
+        $parser = new AutorunsXML13Parser();
+        $parser->parse('unexistredfile210573');
+    }
+
+    public function testParseAutoruns1301XMLt() {
+        $parser = new AutorunsXML13Parser();
+        $parser->parse(dirname(__FILE__). '/../../../fixtures/autoruns13.01-xmlt.xml');
     }
 }
