4b980fec2bc26fcca4e6b970921cef7c3c216035
zhylik <ivan.a.zhilin@ya.ru>
2015-02-23 15:04:42 +0300

    tests

--- tests/Parser/AutorunsXML13Parser.php
+++ tests/Parser/AutorunsXML13Parser.php
@@ -2,7 +2,7 @@
 
 namespace WinWSA\Parser;
 
-require dirname('__FILE__') .'\..\WinWSA\Parser\AutorunsXML13Parser.php';
+require dirname('__FILE__') .'\..\..\WinWSA\Parser\AutorunsXML13Parser.php';
 
 class AutorunsXML13ParserTest extends \PHPUnit_Framework_TestCase
 {
