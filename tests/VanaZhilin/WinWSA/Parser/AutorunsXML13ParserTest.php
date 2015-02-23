c866f280296731596d3fff6aacb22862cb8edac4
vanazhilin <ivan.a.zhilin@ya.ru>
2015-02-23 21:17:33 +0300

    Replace expected and actual at testParseAutoruns1301XMLt

--- tests/VanaZhilin/WinWSA/Parser/AutorunsXML13ParserTest.php
+++ tests/VanaZhilin/WinWSA/Parser/AutorunsXML13ParserTest.php
@@ -17,34 +17,35 @@ class AutorunsXML13ParserTest extends \PHPUnit_Framework_TestCase
         $parser = new AutorunsXML13Parser();
 
         $items = $parser->parse(dirname(__FILE__) .'/../../../fixtures/autoruns13.01-xmlt.xml');
-        $single_item = $items[954];
 
-        $this->assertEquals(21, count($single_item));
-        $this->assertEquals('HKCU\Software\Microsoft\Internet Explorer\UrlSearchHooks', $single_item['vnd_location']);
-        $this->assertEquals('Microsoft Url Search Hook', $single_item['vnd_itemname']);
-        $this->assertEquals('Enabled', $single_item['vnd_enabled']);
-        $this->assertEquals('LOCALHOST\Администратор', $single_item['vnd_profile']);
-        $this->assertEquals('HKCR\CLSID\{CFBFAE00-17A6-11D0-99CB-00C04FD64497}', $single_item['vnd_launchstring']);
-        $this->assertEquals('Интернет-обозреватель', $single_item['vnd_description']);
-        $this->assertEquals('Microsoft Corporation', $single_item['vnd_company']);
-        $this->assertEquals('(Verified) Microsoft Windows', $single_item['vnd_signer']);
-        $this->assertEquals('8.0.7600.16385', $single_item['vnd_version']);
-        $this->assertEquals('20090714-010616', $single_item['vnd_time']);
-        $this->assertEquals('user', $single_item['scope']);
-        $this->assertEquals('LOCALHOST', $single_item['user_domain']);
-        $this->assertEquals('Администратор', $single_item['user_name']);
-        $this->assertEquals('verified', $single_item['vnd_signer_verified']);
-        $this->assertEquals('Microsoft Windows', $single_item['vnd_signer_name']);
-        $this->assertEquals('c:\windows\system32\ieframe.dll', $single_item['vnd_imagepath']);
-        $this->assertEquals('672ecbb050f17bf90fe00758596f38ca', $single_item['vnd_md5hash']);
-        $this->assertEquals('092312ab1ec7dc252795f00e81569994a23356f2', $single_item['vnd_sha1hash']);
-        $this->assertEquals('692ca91d62b65e562a931bc1437b639658ce65c5290a013b73f051a948ae17f9', $single_item['vnd_pesha1hash']);
-        $this->assertEquals('CF739C5992CAADC5C6730501230B792A8A6C3ED5', $single_item['vnd_pesha256hash']);
-        $this->assertEquals('B91A1C93F85AB7E2611AEB097D9B94AE0680E3AB39ACD0D41E16A65DFFDB4733', $single_item['vnd_sha256hash']);
-        $this->assertEquals('0cae1d927cbf8ecaa7e7974a1a04c323', md5(serialize($items)));
+        $single_item = $items[954];
+        $this->assertEquals(count($single_item), 21);
+        $this->assertEquals($single_item['vnd_location'],     'HKCU\Software\Microsoft\Internet Explorer\UrlSearchHooks');
+        $this->assertEquals($single_item['vnd_itemname'],     'Microsoft Url Search Hook');
+        $this->assertEquals($single_item['vnd_enabled'],      'Enabled');
+        $this->assertEquals($single_item['vnd_profile'],      'LOCALHOST\Администратор');
+        $this->assertEquals($single_item['vnd_launchstring'], 'HKCR\CLSID\{CFBFAE00-17A6-11D0-99CB-00C04FD64497}');
+        $this->assertEquals($single_item['vnd_description'],  'Интернет-обозреватель');
+        $this->assertEquals($single_item['vnd_company'],      'Microsoft Corporation');
+        $this->assertEquals($single_item['vnd_signer'],       '(Verified) Microsoft Windows');
+        $this->assertEquals($single_item['vnd_version'],      '8.0.7600.16385');
+        $this->assertEquals($single_item['vnd_time'],         '20090714-010616');
+        $this->assertEquals($single_item['scope'],            'user');
+        $this->assertEquals($single_item['user_domain'],      'LOCALHOST');
+        $this->assertEquals($single_item['user_name'],        'Администратор');
+        $this->assertEquals($single_item['vnd_signer_verified'], 'verified');
+        $this->assertEquals($single_item['vnd_signer_name'],  'Microsoft Windows');
+        $this->assertEquals($single_item['vnd_imagepath'],    'c:\windows\system32\ieframe.dll');
+        $this->assertEquals($single_item['vnd_md5hash'],      '672ecbb050f17bf90fe00758596f38ca');
+        $this->assertEquals($single_item['vnd_sha1hash'],     '092312ab1ec7dc252795f00e81569994a23356f2');
+        $this->assertEquals($single_item['vnd_pesha1hash'],   '692ca91d62b65e562a931bc1437b639658ce65c5290a013b73f051a948ae17f9');
+        $this->assertEquals($single_item['vnd_pesha256hash'], 'CF739C5992CAADC5C6730501230B792A8A6C3ED5');
+        $this->assertEquals($single_item['vnd_sha256hash'],   'B91A1C93F85AB7E2611AEB097D9B94AE0680E3AB39ACD0D41E16A65DFFDB4733');
+
+        $this->assertEquals(md5(serialize($items)), '0cae1d927cbf8ecaa7e7974a1a04c323');
 
         $items = $parser->parse(dirname(__FILE__) .'/../../../fixtures/autoruns13.01-xmltlmods.xml');
-        $this->assertEquals('0cae1d927cbf8ecaa7e7974a1a04c323', md5(serialize($items)));
+        $this->assertEquals(md5(serialize($items)), '0cae1d927cbf8ecaa7e7974a1a04c323');
     }
 
     /**
